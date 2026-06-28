b8fb6c623455146d36e2bb471ebe7365f43ae58a
Yew Toh Wong <yewtohwong@gmail.com>
2026-06-28 00:43:37 +0000

    update

--- activities/activities/Quiz 2.R
+++ activities/activities/Quiz 2.R
@@ -15,25 +15,10 @@ library(readxl)
 
 # Read mod04_blood_pressure.csv
 list.files()
-bp <- read.csv("mod04_blood_pressure.csv")
-t_test <- t.test(bp$dbp, mu = 71, conf.level = 0.99)
+data <- read.csv("Quiz2.csv")
+t_test <- t.test(data$weight, conf.level = 0.95)
 t_test
 
-# default (2-sided test)
-t_std <- t.test(bp$dbp, mu = 71)
-t_std
+# alternative use descriptive
+descriptives(data=data, vars=weight, ci=TRUE, se=TRUE)
 
-# Activities 4.1 ---------------------------------------------------------
-# Average Australian 8700KJ/d
-# Average male aged 30-50 9461KJ (ci:8879-10043)
-# The average male consumed more than the average Australian but ci cannot quantify
-
-# Activity 4.2 -----------------------------------------------------------
-# read Activity_4.2.csv
-list.files()
-health <- read.csv("Activity_4.2.csv")
-
-# Australian female waist circumference 87.9cm
-# Compare the GP clinic sample 
-t.health <- t.test(health$waist, mu=87.9)
-t.health
